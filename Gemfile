78bda09f1f567aba2160327db316a8341f2c4dda
mmatotan <marko.matotan@kodius.com>
2022-11-15 19:09:34 +0100

    Require password confirmation, schema for user registration

--- Gemfile
+++ Gemfile
@@ -44,6 +44,7 @@ gem 'rswag'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'faker'
   gem 'rspec-rails', '~> 6.0.0'
   gem 'ruby-debug-ide', require: false
   gem 'solargraph', require: false
--- spec/requests/api/devise_spec.rb
+++ spec/requests/api/devise_spec.rb
@@ -8,14 +8,58 @@ describe 'Users API' do
       parameter name: :user, in: :body, schema: {
         type: :object,
         properties: {
-          email: { type: :string },
-          password: { type: :string }
-        },
-        required: %w[email password]
+          user: {
+            type: :object,
+            properties: {
+              email: { type: :string, format: :email },
+              password: { type: :string, format: :password },
+              password_confirmation: { type: :string, format: :password }
+            },
+            required: %w[email password password_confirmation]
+          }
+        }
       }
 
-      response '201', 'user created' do
-        let(:user) { { email: 'marko.matotan4@gmail.com', password: 'password123' } }
+      response '201', 'Created' do
+        produces 'application/json'
+        consumes 'application/json'
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 email: { type: :string, format: :email },
+                 created_at: { type: :string, format: 'date-time' },
+                 updated_at: { type: :string, format: 'date-time' }
+               }
+
+        let(:user) do
+          { user: { email: 'marko.matotan4@gmail.com', password: 'password123', password_confirmation: 'password123' } }
+        end
+        run_test!
+      end
+
+      response '422', 'Unprocessable Entity' do
+        produces 'application/json'
+        consumes 'application/json'
+        schema type: :object,
+               properties: {
+                 errors: {
+                   type: :object,
+                   properties: {
+                     invalid_field: {
+                       type: :array,
+                       items: {
+                         type: :string,
+                         example: "can't be blank",
+                         description: 'Error message'
+                       }
+                     }
+                   }
+                 }
+               }
+
+        let(:user) do
+          { user: { email: 'marko.matotan4@gmail.com', password: 'password123' } }
+        end
         run_test!
       end
     end
